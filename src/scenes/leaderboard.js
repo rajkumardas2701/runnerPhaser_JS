7629d69528e8d1bc1e49553a6224b4d488791122
rajkumardas2701 <41339878+rajkumardas2701@users.noreply.github.com>
2021-01-19 15:35:58 +0530

    adding navigations on Leaderboard scene

--- src/scenes/leaderboard.js
+++ src/scenes/leaderboard.js
@@ -10,5 +10,29 @@ export default class LeaderBoard extends Phaser.Scene {
       fontSize: '48px',
       fontFamily: 'cursive',
     }).setOrigin(0.5);
+
+    const actions = [{
+      imgName: 'menu',
+      xcoord: 150,
+      scene: 'Menu',
+    },
+    {
+      imgName: 'play',
+      xcoord: 500,
+      scene: 'game',
+    },
+    {
+      imgName: 'instructions',
+      xcoord: 850,
+      scene: 'Instructions',
+    }];
+
+    for (let i = 0; i < actions.length; i += 1) {
+      const btn = this.add.image(actions[i].xcoord, 550, actions[i].imgName).setScale(0.7);
+      btn.setInteractive();
+      btn.on('pointerup', () => {
+        this.scene.start(`${actions[i].scene}`);
+      });
+    }
   }
 }
